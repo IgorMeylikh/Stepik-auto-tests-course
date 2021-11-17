1b85dbda747a4b4478f16b43078d52ea74e5644e
Igor Meylikh <dream_killer2007@mail.ru>
2021-11-17 15:43:58 +0300

    Добавил в файл gecko.py возможность по очереди запускать браузеры

--- Lesson_3_6/gecko.py
+++ Lesson_3_6/gecko.py
@@ -1,6 +1,31 @@
-from selenium import webdriver
+# from selenium import webdriver
 
 # инициализируем драйвер браузера. После этой команды вы должны увидеть новое открытое окно браузера
-driver = webdriver.Firefox()
-#driver = webdriver.Firefox(executable_path=r'C:\geckodriver\geckodriver.exe')
-driver.get("https://stepik.org/lesson/25969/step/8")
+# driver = webdriver.Firefox()
+# driver = webdriver.Firefox(executable_path=r'C:\geckodriver\geckodriver.exe')
+# driver.get("https://stepik.org/lesson/25969/step/8")
+
+
+
+#Простой вариант
+# link = "http://selenium1py.pythonanywhere.com/"
+
+# def test_guest_should_see_login_link(browser):
+#     browser.get(link)
+#     browser.find_element_by_css_selector("#login_link")
+#######################################################
+
+
+# Параметризация тестов для выбора браузера
+
+import pytest
+from selenium import webdriver
+
+link = "http://selenium1py.pythonanywhere.com/"
+drivers = ['webdriver.Chrome()', 'webdriver.Firefox()']
+
+@pytest.mark.parametrize('browser', drivers)
+def test_guest_should_see_login_link(browser):
+    with eval(browser) as br:
+        br.get(link)
+        br.find_element_by_css_selector("#login_link")
